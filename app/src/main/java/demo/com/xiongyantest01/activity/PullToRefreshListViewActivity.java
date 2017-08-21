548e46bafc91a1645ac0b406fa842387676c7d14
xiongyan <xiongyan@111.com.cn>
2017-08-21 15:06:18 +0800

    pullToRefreshListView

---
+++ app/src/main/java/demo/com/xiongyantest01/activity/PullToRefreshListViewActivity.java
@@ -0,0 +1,27 @@
+package demo.com.xiongyantest01.activity;
+
+/**
+ * Created by xiongyan on 2017/8/17.
+ */
+
+public class PullToRefreshListViewActivity extends BaseActivity {
+    @Override
+    protected int setLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initIntent() {
+
+    }
+}
