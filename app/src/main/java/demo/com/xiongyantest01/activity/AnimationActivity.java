8f7446559ee1e056a54d06fe4db028bd10374e36
xiongyan <xiongyan@111.com.cn>
2017-11-13 15:34:25 +0800

    同一个textView加粗指定字符串

--- app/src/main/java/demo/com/xiongyantest01/activity/AnimationActivity.java
+++ app/src/main/java/demo/com/xiongyantest01/activity/AnimationActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.Html;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.TextAppearanceSpan;
@@ -19,7 +20,7 @@ import demo.com.xiongyantest01.R;
  */
 
 public class AnimationActivity extends Activity {
-    TextView demo,demo1,demo2,demo3;
+    TextView demo, demo1, demo2, demo3, demo4;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,6 +30,8 @@ public class AnimationActivity extends Activity {
         demo1 = (TextView) findViewById(R.id.demo1);
         demo2 = (TextView) findViewById(R.id.demo2);
         demo3 = (TextView) findViewById(R.id.demo3);
+        demo4 = (TextView) findViewById(R.id.demo4);
+        demo4.setText(Html.fromHtml(getTitleText()));
         demo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +39,18 @@ public class AnimationActivity extends Activity {
             }
         });
         setText();
+    }
+
+    private String getTitleText() {
+        String ss = "你所搜索的艾泰为是处方药，武汉有30家店铺为你提供佩服";
+        String s1 = "艾泰为";
+        String s2 = "武汉";
+        String s3 = "30家店铺";
+        ss = ss.replace(s1, "<b>" + s1 + "</b>").replace(s2, "<b>" + s2 + "</b>").replace(s3, "<b>" + s3 + "</b>");
+//        ss = ss.replace(s2, "<b>" + s2 + "</b>");
+//        ss = ss.replace(s3, "<b>" + s3 + "</b>");
+        return ss;
+
     }
 
 
@@ -48,21 +63,23 @@ public class AnimationActivity extends Activity {
 
     private void setText() {
         SpannableStringBuilder spanBuilder = new SpannableStringBuilder(getResources().getString(R.string.vip_un_login_esc));
-        spanBuilder.setSpan(new TextAppearanceSpan(null, 0, 30, null, null), 4, spanBuilder.length () , Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        spanBuilder.setSpan(new TextAppearanceSpan(null, 0, 30, null, null), 4, spanBuilder.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
         demo.setText(spanBuilder);
 
         SpannableStringBuilder spanBuilder1 = new SpannableStringBuilder(getResources().getString(R.string.vip_not));
-        spanBuilder1.setSpan(new TextAppearanceSpan(null, 0, 30, null, null), 4, spanBuilder1.length() , Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        spanBuilder1.setSpan(new TextAppearanceSpan(null, 0, 30, null, null), 4, spanBuilder1.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
         demo1.setText(spanBuilder1);
 
 
         SpannableStringBuilder spanBuilder2 = new SpannableStringBuilder(getResources().getString(R.string.vip_is_esc));
-        spanBuilder2.setSpan(new TextAppearanceSpan(null, 0, 30, null, null), 4, spanBuilder2.length() , Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        spanBuilder2.setSpan(new TextAppearanceSpan(null, 0, 30, null, null), 4, spanBuilder2.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
         demo2.setText(spanBuilder2);
 
         SpannableStringBuilder spanBuilder3 = new SpannableStringBuilder(getResources().getString(R.string.vip_is_expire));
-        spanBuilder3.setSpan(new TextAppearanceSpan(null, 0, 30, null, null), 4, spanBuilder3.length() , Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
+        spanBuilder3.setSpan(new TextAppearanceSpan(null, 0, 30, null, null), 4, spanBuilder3.length(), Spanned.SPAN_EXCLUSIVE_INCLUSIVE);
         demo3.setText(spanBuilder3);
 
     }
+
+
 }
